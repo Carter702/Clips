e92f2d9bfa27111b6dd689d229a1f5f96c9b10ce
Khyri Carter <cartek3@unlv.nevada.edu>
2018-03-02 03:35:00 -0800

    fixed add list appending to front

--- js/popup.js
+++ js/popup.js
@@ -1,20 +1,27 @@
 // popup.js
 
+var listCount = 3;
 
-
-
- document.addEventListener('DOMContentLoaded', function() {
-    var mySubmitButton = document.getElementById('l1');
-    // onClick's logic below:
-    mySubmitButton.addEventListener('click', function() {
+// Add item to listed when clicked
+document.addEventListener('DOMContentLoaded', function () {
+    var myList = document.getElementById('myUL');
+    
+    // onClick
+    myList.addEventListener('click', function () {
+        changeText();
         addList();
     });
 });
 
- function addList() {
-  var ul = document.getElementById("myUL");
-  var li = document.createElement("li");
-  li.appendChild(document.createTextNode("Add New Clip..."));
-  li.setAttribute("id", "element4"); // added line
-  ul.appendChild(li);
- } 
+function addList() {
+    var ul = document.getElementById("myUL");
+    var li = document.createElement("li");
+    li.appendChild(document.createTextNode("Add New Clip..."));
+    li.setAttribute("id", "l2");
+    ul.appendChild(li);
+}
+
+function changeText() {
+    var l = document.getElementById("l1");
+    l.textContent = "HELLO";
+}
